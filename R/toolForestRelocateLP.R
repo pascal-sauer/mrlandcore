777aab89c212b8022260418879dfa2d540ac40d2
Pascal Sauer <156898545+pascal-sauer@users.noreply.github.com>
2026-03-13 16:14:09 +0100

    dense constraint matrix

--- R/toolForestRelocateLP.R
+++ R/toolForestRelocateLP.R
@@ -24,8 +24,12 @@ toolForestRelocateLP <- function(lu, natTarget, vegC) {
 }
 
 toolForestRelocateCountry <- function(lu, natTarget) {
-  stopifnot(length(getItems(lu, "iso")) == 1)
+  stopifnot(length(getItems(lu, "iso")) == 1,
+            dim(natTarget)[1] == 1,
+            getItems(lu, 2) == getItems(natTarget, 2),
+            getItems(lu, 3) == getItems(natTarget, 3))
   luTotal <- dimSums(lu, 3)
+  years <- getItems(lu, 2)
 
   # lp variables
   v <- rbind(expand.grid(getItems(lu, 1), getItems(lu, 2), getItems(lu, 3), stringsAsFactors = FALSE),
@@ -33,7 +37,7 @@ toolForestRelocateCountry <- function(lu, natTarget) {
              expand.grid("slackNegative", getItems(lu, 2), getItems(lu, 3), stringsAsFactors = FALSE))
   colnames(v) <- c("cell", "year", "landtype")
 
-  # get lp variable id (row) number
+  # get lp variable id number
   vid <- function(cell, year, landtype) {
     if (missing(cell) || is.null(cell)) {
       return(which(v$year == year & v$landtype == landtype))
@@ -43,53 +47,76 @@ toolForestRelocateCountry <- function(lu, natTarget) {
       return(which(v$cell == cell & v$year == year & v$landtype == landtype))
     }
   }
-  constraints <- matrix(nrow = 0, ncol = nrow(v))
-  rightHandSide <- c()
-  constraintsDirection <- c()
+
+  nConstraints1 <- nyears(lu) * ndata(lu)
+  nConstraints2 <- nyears(lu) * ncells(lu)
+  nConstraints3 <- (nyears(lu) - 1) * ncells(lu)
+  nConstraints <- nConstraints1 + nConstraints2 + nConstraints3
+
+  # dense constraint matrix: constraint number, column/variable id number, value
+  constraints <- array(dim = c(0, 3))
+  rightHandSide <- rep(NA, nConstraints)
+  constraintsDirection <- rep(NA, nConstraints)
+
+  # index of the next constraint to add; independent of row number of `constraints`, because that's a dense matrix
+  iConstraint <- 1
 
   for (y in seq_len(nyears(natTarget))) {
-    year <- getYears(natTarget)[y]
-    message(Sys.time(), " - ", year)
+    message(Sys.time(), " - ", years[y])
 
-    # 1. country level: total of each landtype should match natTarget
+    # 1. sum_over_cells(v[, y, landtype]) + v["slackPositive", y, landtype] - v["slackNegative", y, landtype]
+    #    == natTarget[, y, landtype]
+    # country level: total of each landtype should match natTarget
     for (landtype in getItems(lu, 3)) {
-      equation <- matrix(0, nrow = 1, ncol = nrow(v))
-      equation[, vid(, year, landtype)] <- 1
-      equation[, vid("slackNegative", year, landtype)] <- -1
-      constraints <- rbind(constraints, equation)
-      constraintsDirection <- c(constraintsDirection, "==")
-      rightHandSide <- c(rightHandSide, natTarget[, y, landtype])
+      newConstraint <- array(dim = c(ncells(lu) + 2, 3))
+      newConstraint[, 1] <- iConstraint
+      newConstraint[, 2] <- vid(, years[y], landtype)
+      newConstraint[, 3] <- ifelse(v$cell[newConstraint[, 2]] == "slackNegative", -1, 1)
+      constraints <- rbind(constraints, newConstraint)
+
+      constraintsDirection[iConstraint] <- "=="
+      rightHandSide[iConstraint] <- natTarget[, y, landtype]
+      iConstraint <- iConstraint + 1
     }
     message(Sys.time(), " - 1. added")
-
-    # 2. cell level: total nature (primf+secdf+forestry+other) must match lu
-    equations <- matrix(0, nrow = ncells(lu), ncol = nrow(v))
+    # 2. sum(v[cell, y, ]) == luTotal[, y, ]
+    # cell level: total nature (primf+secdf+forestry+other) must match lu
     for (i in seq_len(ncells(lu))) {
-      equations[i, vid(getItems(lu, 1)[i], year, )] <- 1
+      constraints <- rbind(constraints,
+                           cbind(iConstraint + i - 1,
+                                 vid(getItems(lu, 1)[i], years[y], ),
+                                 1))
     }
-    constraints <- rbind(constraints, equations)
-    constraintsDirection <- c(constraintsDirection, rep("==", nrow(equations)))
-    rightHandSide <- c(rightHandSide, luTotal[, y, ])
+    nConstraintsAdded <- ncells(lu)
+
+    constraintsDirection[iConstraint:(iConstraint + nConstraintsAdded - 1)] <- "=="
+    rightHandSide[iConstraint:(iConstraint + nConstraintsAdded - 1)] <- luTotal[, y, ]
+    iConstraint <- iConstraint + nConstraintsAdded
     message(Sys.time(), " - 2. added")
 
-    # 3. primf cannot be larger than in previous timestep
+    # 3. v[cell, y, primf] - v[cell, y - 1, primf] <= 0
+    # cell level: primf cannot be larger than in previous timestep
     if (y > 1) {
-      equations <- matrix(0, nrow = ncells(lu), ncol = nrow(v))
-      i <- 1
-      for (cell in getItems(lu, 1)) {
-        equations[i, vid(cell, year, "primforest")] <- 1
-        i <- i + 1
+      for (i in seq_len(ncells(lu))) {
+        constraints <- rbind(constraints,
+                             cbind(iConstraint + i - 1,
+                                   c(vid(getItems(lu, 1)[i], years[y], "primforest"),
+                                     vid(getItems(lu, 1)[i], years[y - 1], "primforest")),
+                                   c(1, -1)))
       }
-      constraints <- rbind(constraints, equations)
-      constraintsDirection <- c(constraintsDirection, rep("<=", nrow(equations)))
-      rightHandSide <- c(rightHandSide, lu[, y - 1, "primforest"])
+      nConstraintsAdded <- ncells(lu)
+
+      constraintsDirection[iConstraint:(iConstraint + nConstraintsAdded - 1)] <- "<="
+      rightHandSide[iConstraint:(iConstraint + nConstraintsAdded - 1)] <- 0
+      iConstraint <- iConstraint + nConstraintsAdded
+      message(Sys.time(), " - 3. added")
     }
   }
-  message(Sys.time(), " - 3. added")
 
+  message(Sys.time(), " - starting solve...")
   solution <- lpSolve::lp(direction = "min",
                           objective.in = ifelse(v$cell == "slack", 1, 0),
-                          const.mat = constraints,
+                          dense.const = constraints,
                           const.dir = constraintsDirection,
                           const.rhs = rightHandSide)
   message(Sys.time(), " - solved")
